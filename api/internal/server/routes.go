f2e1b08cc84f63f9d1b8df50758223f6bb17eb4b
Damilola O. <82976159+ddddami@users.noreply.github.com>
2026-05-24 20:22:15 +0100

    Remove deprecated middleware.RealIP

--- api/internal/server/routes.go
+++ api/internal/server/routes.go
@@ -11,7 +11,6 @@ func (app *app) routes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
 	r.NotFound(app.notFoundResponse)
 	r.MethodNotAllowed(app.methodNotAllowedResponse)
