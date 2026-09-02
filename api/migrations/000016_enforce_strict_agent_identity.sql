e2321f81f62d9bdda627fc0fa4f3498e2cccb07d
Damilola O. <82976159+ddddami@users.noreply.github.com>
2026-09-02 19:15:08 +0100

    db: Enforce strict agent identity

---
+++ api/migrations/000016_enforce_strict_agent_identity.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- +goose StatementBegin
+DELETE FROM agent_offers WHERE agent_id IN (SELECT id FROM agents WHERE user_id IS NULL);
+DELETE FROM agent_campuses WHERE agent_id IN (SELECT id FROM agents WHERE user_id IS NULL);
+DELETE FROM agents WHERE user_id IS NULL;
+ALTER TABLE agents ALTER COLUMN user_id SET NOT NULL;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE agents ALTER COLUMN user_id DROP NOT NULL;
+-- +goose StatementEnd
